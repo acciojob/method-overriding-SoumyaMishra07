26d8075e952f70adc20f8abf97c5a82bf55d353a
SoumyaMishra07 <soumyarmishra065@gmail.com>
2024-02-12 13:17:42 +0530

    added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
-
+class A {
+    // Method meth in class A
+    String meth() {
+        return "Invoking method from class A";
+    }
+}
+class B extends A {
+    // Overriding method meth in class B
+    @Override
+    String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
 public class Main {
-  
+    public static void main(String[] args) {
+        B objB = new B();
+        // Calling method meth from class A using object of class B
+        System.out.println(objB.meth());
+        // Calling overridden method meth from class B
+        // This will print "Method is overridden in Extended class B"
+        System.out.println(objB.meth());
+    }
 }
